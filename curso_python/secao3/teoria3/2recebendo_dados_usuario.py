36e330c5a516183509e6188a4f2cdbf5955006b4
moderno <tiago_figueira@hotmail.com>
2022-07-21 19:45:44 -0300

    primeiro commit

--- curso_python/secao3/teoria3/2recebendo_dados_usuario.py
+++ curso_python/secao3/teoria3/2recebendo_dados_usuario.py
@@ -1,8 +1,30 @@
 """
-Recebendo dados do usuário:
-"""
 
+Recebendo dados do usuário:
+Usando a versão antiga o python
+# Entrada de dados
 print("Qual é o seu nome?")
 nome = input()
-
 print('Seja bem-vindo(a) %s' % nome)
+
+print("Qual sua idade?")
+idade = input()
+
+# Processamento
+
+#Saída
+print('A %s tem %s anos'% (nome, idade))
+
+"""
+# Entrada de dados
+print("Qual é o seu nome?")
+nome = input()
+print('Seja bem-vindo(a) %s' % nome)
+
+print("Qual sua idade?")
+idade = input()
+
+# Processamento
+
+#Saída
+print('A %s tem %s anos'% (nome, idade))
